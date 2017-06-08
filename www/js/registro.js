98bdad01a6339c44e51baaeb310e60c1a876f4ac
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-06-08 15:25:31 -0500

    Se ajustó el registro de usuarios en el servidor, falta hacerlo localmente

--- www/js/registro.js
+++ www/js/registro.js
@@ -4,150 +4,197 @@
 
 angular.module('starter.registro', ['ionic'])
 
-.controller('registroController',function ($scope, $state,$ionicPopup, $http, $ionicLoading, $cordovaSQLite, $ionicPlatform) {
+    .controller('registroController', function ($scope, $state, $ionicPopup,
+                                                $http, $ionicLoading,
+                                                $cordovaSQLite, $ionicPlatform,
+                                                $q) {
 
-    $scope.registro="hola"
+        var modulePromises = [];
+        var correoDisponible = false;
+        var registroCorrecto = false;
 
-    $scope.goInicio = function(){
-        $state.go('inicio')
-    };
+        // $scope.registroData = {
+        //     correo: '',
+        //     password: '',
+        //     passwordConf: ''
+        // };
 
-    $scope.goRegistro = function(){
-        $state.go('inicio')
-    };
+        $scope.registroData = {
+            correo: 'porfirioads@gmail.com',
+            password: 'holamundo',
+            passwordConf: 'holamundo'
+        };
 
-    $scope.goLogin = function(){
-        $state.go('login')
-    };
+        $scope.goInicio = function () {
+            $state.go('inicio')
+        };
 
+        $scope.goRegistro = function () {
+            $state.go('inicio')
+        };
 
+        $scope.goLogin = function () {
+            $state.go('login')
+        };
 
+        $scope.registrarCuenta = function () {
+            var datosCompletos = false;
+            var correo = $scope.registroData.correo;
+            var password = $scope.registroData.password;
+            var passwordConf = $scope.registroData.passwordConf;
 
-    $scope.registro = function (correo, password, passwordConf) {
-        console.log(correo,password,passwordConf);
-        var datos = false;
-        if(correo==null || password==null || passwordConf==null ){
-            popup("Campos Vacios","Llenar todos los campos vacios.")
-        }else{
-            if(password!=passwordConf){
-                popup("Contraseñas incorrectas","Verificar las contraseñas.")
-            }else{
-                datos = true;
+            if (correo == null || password == null || passwordConf == null) {
+                popup('Error', 'Debes llenar todos los campos');
+            } else {
+                if (password != passwordConf) {
+                    popup('Error', 'Las contraseñas no coinciden');
+                } else {
+                    datosCompletos = true;
+                }
             }
-        }
 
-        if(datos){
-            showLoading();
-            consultarCorreo(0)
-        }
+            if (datosCompletos) {
+                showLoading();
+                consultarCorreo(1);
 
-    };
-
-    function consultarCorreo(intento) {
-
-        var httpRequest = {
-            method: 'POST',
-            url: 'http://192.168.1.74:8000/usuario_exists',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            transformRequest: function(obj) {
-                var str = [];
-                for(var p in obj)
-                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-                return str.join("&");
-            },
-            data: {email: $scope.registro.correo},
-            timeout: 3000
+                $q.all(modulePromises).then(function () {
+                    if (correoDisponible) {
+                        insertarUsuario(1);
+                        $q.all(modulePromises).then(function () {
+                            if (registroCorrecto) {
+                                popup('Ok', 'Me registré, ahora puedo usar GolfApp');
+                            }
+                        });
+                    }
+                });
+            }
         };
 
-        $http(httpRequest)
-            .then(function successCallback(response) {
+        function createPostHttpRequest(url, data) {
+            var httpRequest = {
+                method: 'POST',
+                url: url,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                transformRequest: function (obj) {
+                    var str = [];
+                    for (var p in obj)
+                        str.push(encodeURIComponent(p) + "="
+                            + encodeURIComponent(obj[p]));
+                    return str.join("&");
+                },
+                data: data,
+                timeout: 3000
+            };
 
-                if(!response.data.existe){
-                        insertarUsuario(0)
-                }else{
-                    popup("Correo ocupado",$scope.registro.correo+ " correo ocupado "+response.data.message)
-                    $ionicLoading.hide()
+            return httpRequest;
+        }
+
+        function consultarCorreo(intento) {
+            var httpRequest = createPostHttpRequest(
+                'http://192.168.43.230:8000/usuario_exists',
+                {email: $scope.registroData.correo}
+            );
+
+            var correoRequest = $http(httpRequest)
+                .then(function successCallback(response) {
+                    if (response.data.existe) {
+                        $ionicLoading.hide();
+                        popup('Error', 'El email ya está siendo utilizado');
+                        correoDisponible = false;
+                    } else {
+                        correoDisponible = true;
+                    }
+                }, function errorCallback(response) {
+                    if (response.status == 400) {
+                        popup('Error response', response.data.error_message);
+                        correoDisponible = false;
+                    } else {
+                        if (intento < 3) {
+                            consultarCorreo(intento + 1);
+                        } else {
+                            $ionicLoading.hide();
+                            popup('Error response', 'status: '
+                                + response.status);
+                            correoDisponible = false;
+                        }
+                    }
+                });
+
+            modulePromises.push(correoRequest);
+        }
+
+        function insertarUsuario(intento) {
+            var httpRequest = createPostHttpRequest(
+                'http://192.168.43.230:8000/usuario_insert',
+                {
+                    email: $scope.registroData.correo,
+                    password: $scope.registroData.password
                 }
-            }, function errorCallback(response) {
-                if (intento < 3) {
-                    consultarCorreo(intento + 1);
-                } else {
-                    popup("Error", JSON.stringify(response.data) + "<br>"
-                        + " intentos: " + intento);
+            );
+
+            var insertarRequest = $http(httpRequest)
+                .then(function successCallback(response) {
                     $ionicLoading.hide();
-                }
+                    if (response.data.insertado) {
+                        popup('Registro', 'La cuenta fue creada correctamente');
+                        popup('Registro', 'usuario_id: '
+                            + response.data.usuario_id);
+                        registroCorrecto = true;
+                    } else {
+                        popup('Error de registro', response.data.error_message);
+                        registroCorrecto = false;
+                    }
+                }, function errorCallback(response) {
+                    if (response.status == 400) {
+                        popup('Error response', response.data.error_message);
+                        registroCorrecto = false;
+                    } else {
+                        if (intento < 3) {
+                            insertarUsuario(intento + 1);
+                        } else {
+                            $ionicLoading.hide();
+                            popup('Error response', 'status: '
+                                + response.status);
+                            registroCorrecto = false;
+                        }
+                    }
+                });
+
+            modulePromises.push(insertarRequest);
+        }
+
+        function guardarUsuarioPhone() {
+            var query = "INSERT INTO usuario (id, email, password) VALUES (?,?,?)";
+            $cordovaSQLite.execute(db, query, [1, $scope.registro.correo, $scope.registro.password])
+                .then(function (res) {
+                    popup("Bienvenido a GolfApp", "Ahora ya eres un miembro de GolfApp, Crea partidos y disfruta de privilegios.")
+                    $state.go('inicio')
+                }, function (err) {
+                    popup("Usuario error", " erroe")
+                    console.error(err);
+                });
+        }
+
+        function popup(title, template) {
+            var alertPopup = $ionicPopup.alert({
+                title: title,
+                template: template
             });
-    }
-
-    function insertarUsuario(intento) {
-
-        var httpRequest = {
-            method: 'POST',
-            url: 'http://192.168.1.74:8000/usuario_insert',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            transformRequest: function(obj) {
-                var str = [];
-                for(var p in obj)
-                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-                return str.join("&");
-            },
-            data: {email: $scope.registro.correo, password: $scope.registro.password},
-            timeout: 3000
         };
 
-        $http(httpRequest)
-            .then(function successCallback(response) {
-                $ionicLoading.hide();
-
-
-                if(response.data.code == 200){
-                    guardarUsuarioPhone()//popup("Usuario Insertado","insertado "+ response.data.code + " "+response.data.email +" "+response.data.password)
-                }else{
-                    popup("Usuario NO Insertado"," NO insertado "+ response.data.code)
-                }
-
-            }, function errorCallback(response) {
-                if (intento < 3) {
-                    insertarUsuario(intento + 1);
-                } else {
-                    popup("Error", JSON.stringify(response.data) + "<br>"
-                        + " intentos: " + intento);
-                    $ionicLoading.hide();
-                }
+        function showLoading() {
+            $ionicLoading.show({
+                template: '<ion-spinner></ion-spinner>' +
+                '<p>Cargando</p>',
+                animation: 'fade-in'
+            }).then(function () {
+                // console.log("The loading indicator is now displayed");
             });
-    }
+        };
 
-    function guardarUsuarioPhone() {
-        var query = "INSERT INTO usuario (id, email, password) VALUES (?,?,?)";
-        $cordovaSQLite.execute(db, query, [1, $scope.registro.correo, $scope.registro.password])
-            .then(function (res) {
-                popup("Bienvenido a GolfApp","Ahora ya eres un miembro de GolfApp, Crea partidos y disfruta de privilegios.")
-                $state.go('inicio')
-        }, function (err) {
-                popup("Usuario error"," erroe")
-                console.error(err);
-            });
-    };
 
-    function popup(title, template) {
-        var alertPopup = $ionicPopup.alert({
-            title: title,
-            template: template
+        $ionicPlatform.ready(function () {
+            console.log('jugadoresController', 'Ready');
+            ;
         });
-    };
-
-    function showLoading() {
-        $ionicLoading.show({
-            template: '<ion-spinner></ion-spinner>' +
-            '<p>Cargando</p>',
-            animation: 'fade-in'
-        }).then(function () {
-            // console.log("The loading indicator is now displayed");
-        });
-    };
-
-
-    $ionicPlatform.ready(function () {
-        console.log('jugadoresController', 'Ready');;
     });
-});
