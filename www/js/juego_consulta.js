22c5dca71d5c025cb61f51bffadbe5110f297cfa
Victor Hugo <victorhugo_reveles@hotmail.com>
2017-12-30 18:22:40 -0600

    Se añadieron las posiciones de los jugadores en el juego consulta

--- www/js/juego_consulta.js
+++ www/js/juego_consulta.js
@@ -81,6 +81,7 @@ angular.module('starter.juego_consulta', ['ionic'])
 
                         setTimeout(function () {
                             fixRowsAndColumns();
+                            calcularPosiciones();
                         }, 1000);
                     } else {
                         utils.popup('Error',
@@ -158,6 +159,57 @@ angular.module('starter.juego_consulta', ['ionic'])
                 }, function (err) {
                     // console.log(JSON.stringify(err))
                 });
+        }
+
+        function compare(a, b) {
+            if (a.golpes < b.golpes)
+                return -1;
+            if (a.golpes > b.golpes)
+                return 1;
+            return 0;
+        }
+
+        function ordinal_suffix_of(i) {
+            var j = i % 10,
+                k = i % 100;
+            if (j == 1 && k != 11) {
+                return i + "st";
+            }
+            if (j == 2 && k != 12) {
+                return i + "nd";
+            }
+            if (j == 3 && k != 13) {
+                return i + "rd";
+            }
+            return i + "th";
+        }
+
+        function calcularPosiciones() {
+            var array = [];
+            for (var i = 0; i < $scope.tablero.datos_juego.length; i++) {
+
+                if ($scope.tablero.datos_juego[i].nombre.toLowerCase() != "mudo") {
+                    array.push({
+                        idx: $scope.tablero.datos_juego[i].index,
+                        golpes: $scope.tablero.datos_juego[i].totales_golpes[3]
+                    })
+                }
+            }
+
+            array = array.sort(compare)
+            var posiciones = 1;
+            var ordinalNumber=0;
+
+            $scope.tablero.datos_juego[array[0].idx].lugar = ordinal_suffix_of(posiciones);
+
+            for(var i=1; i<array.length;i++){
+                if(array[i].golpes != array[i-1].golpes){
+                    posiciones = i+1
+                }
+                ordinalNumber = ordinal_suffix_of(posiciones);
+                $scope.tablero.datos_juego[array[i].idx].lugar =  ordinalNumber;
+
+            }
         }
 
     });
